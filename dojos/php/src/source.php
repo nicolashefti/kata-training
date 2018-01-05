e6b2bdb612a1ef006a2a9c24fce0fce85e9e4719
Nicolas Hefti <nicolashefti@users.noreply.github.com>
2018-01-05 15:51:19 +0100

    Update source.php

--- dojos/php/src/source.php
+++ dojos/php/src/source.php
@@ -1,37 +1,3 @@
-# Dumpbox
+<?php
 
-## Intro
-
-Dumpbox is simple uploading platform allowing people to share easily pictures made on their mobile.
-
-## Guidelines
-
-- Blind PHP backend providing all the service through a JSON API
-- Standalone frontend (no framework required) using Bootstrap
-- Mobile friendly
-- Make sure the application works out-of-the box once you're done
-- Use Docker to facilitate the setup of the application
-- Add a short description on how to run your program in the *Setup* section bellow
-- Readable, extendable and clean code
-- Basic unit testing (at least for Backend)
-
-## Setup
-
-...
-
-- Mobile friendly
-- Make sure the application works out-of-the box once you're done
-- Use Docker to facilitate the setup of the application
-- Add a short description on how to run your program in the *Setup* section bellow
-- Readable, extendable and clean code
-- Basic unit testing (at least for Backend)
-
-## Setup
-- Mobile friendly
-- Make sure the application works out-of-the box once you're done
-- Use Docker to facilitate the setup of the application
-- Add a short description on how to run your program in the *Setup* section bellow
-- Readable, extendable and clean code
-- Basic unit testing (at least for Backend)
-
-## Setup
+// Nothing to declare
